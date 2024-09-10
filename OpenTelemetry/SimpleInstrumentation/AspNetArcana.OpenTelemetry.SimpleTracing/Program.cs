6db310bcfd285b6a3ece7016f0e4bb238a2287bb
przemyslawkleszcz <kleszcz@mail.com>
2024-09-10 18:32:29 +0200

    add project to the new location/setup jaeger/add instrumentation

---
+++ OpenTelemetry/SimpleInstrumentation/AspNetArcana.OpenTelemetry.SimpleTracing/Program.cs
@@ -0,0 +1,55 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(x => x.AddService("AspNetArcana.OpenTelemetry.SimpleTracing"))
+    .WithTracing(x => x.AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddOtlpExporter(x => x.Endpoint = new Uri("http://jaeger:4317")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    Activity.Current?.SetTag("client.id", 69);
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ OpenTelemetry/SimpleInstrumentation/AspNetArcana.OpenTelemetry.SimpleTracing/ReadMe.md
@@ -0,0 +1,28 @@
+﻿Install Required Packages
+
+To begin, you'll need to install the following packages:
+
+```bash
+dotnet add package OpenTelemetry.Extensions.Hosting
+dotnet add package OpenTelemetry.Instrumentation.AspNetCore
+dotnet add package OpenTelemetry.Instrumentation.Http
+dotnet add package OpenTelemetry.Exporter.Console
+dotnet add package OpenTelemetry.Exporter.OpenTelemetryProtocol
+```
+
+Setting Up Instrumentation in Program.cs
+
+You can configure OpenTelemetry instrumentation in your Program.cs file. For example, to add an additional tag for tracing, you can use the following code snippet:
+
+```csharp
+Activity.Current?.SetTag("client.id", 69);
+```
+
+To add more tags for tracing, simply repeat the SetTag method like this:
+
+```csharp
+Activity.Current?.SetTag("client.id", 69);
+Activity.Current?.SetTag("custom.tag", "your_value");
+```
+
+Make sure you have the correct setup in Program.cs to initialize OpenTelemetry tracing.
