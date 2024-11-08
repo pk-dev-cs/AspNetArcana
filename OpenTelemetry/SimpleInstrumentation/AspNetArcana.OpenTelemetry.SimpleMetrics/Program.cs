c490894ff0e3055778c3fb876bafa3873cc1f795
przemyslawkleszcz <kleszcz@mail.com>
2024-11-08 18:51:26 +0100

    add project for simple metrics measurement

---
+++ OpenTelemetry/SimpleInstrumentation/AspNetArcana.OpenTelemetry.SimpleMetrics/Program.cs
@@ -0,0 +1,61 @@
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using AspNetArcana.OpenTelemetry.SimpleMetrics.Diagnostics;
+using OpenTelemetry.Metrics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(x => x.AddService("AspNetArcana.OpenTelemetry.SimpleTracing"))
+    .WithMetrics(metrics =>
+        metrics
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddMeter(ApplicationDiagnostics.Meter.Name)
+            .AddConsoleExporter()
+            .AddPrometheusExporter());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    ApplicationDiagnostics.ClientsCreatedCounter.Add(1);
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
