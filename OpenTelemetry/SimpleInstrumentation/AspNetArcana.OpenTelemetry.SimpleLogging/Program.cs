69797de12f918f0f5429870a2d44bc8c646952cd
przemyslawkleszcz <kleszcz@mail.com>
2024-11-20 00:15:21 +0100

    add project for logging

---
+++ OpenTelemetry/SimpleInstrumentation/AspNetArcana.OpenTelemetry.SimpleLogging/Program.cs
@@ -0,0 +1,64 @@
+using Serilog;
+using Serilog.Sinks.Grafana.Loki;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.GrafanaLoki(
+        "http://loki:3100",
+        new[]
+        {
+            new LokiLabel { Key = "app", Value = "simplelogging" },
+            new LokiLabel { Key = "environment", Value = "development" }
+        })
+    .CreateLogger();
+
+builder.Logging.ClearProviders(); // Remove default logging providers
+builder.Logging.AddSerilog();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    Log.Information("Application started!");
+    Log.Warning("This is a warning log.");
+    Log.Error("This is an error log.");
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
