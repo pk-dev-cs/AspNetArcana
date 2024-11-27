3db79279cf3936a3e203e716fb0d44762d923c80
przemyslawkleszcz <kleszcz@mail.com>
2024-11-27 20:09:14 +0100

    add an example basic logging

---
+++ Logging/AspNetArcana.Logging.BasicLogging/Program.cs
@@ -0,0 +1,15 @@
+﻿using AspNetArcana.Logging.BasicLogging;
+using Microsoft.Extensions.Logging;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddJsonConsole();
+    builder.AddProvider(new CustomLoggerProvider());
+});
+
+ILogger logger = loggerFactory.CreateLogger<Program>();
+
+var name = "Adam";
+var age = 32;
+
+logger.LogInformation("{Name} just turned {Age}", name, age);
