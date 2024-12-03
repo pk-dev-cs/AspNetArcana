84c30f634700cd9a48c3b681fef9b7027872027f
przemyslawkleszcz <kleszcz@mail.com>
2024-12-03 17:02:24 +0100

    add timed operation example

--- Logging/DotnetArcana.Logging.BasicLogging/Program.cs
+++ Logging/DotnetArcana.Logging.BasicLogging/Program.cs
@@ -1,9 +1,16 @@
 ﻿using AspNetArcana.Logging.BasicLogging;
+using DotnetArcana.Logging.BasicLogging;
 using Microsoft.Extensions.Logging;
 
 using var loggerFactory = LoggerFactory.Create(builder =>
 {
-    builder.AddJsonConsole();
+    builder.AddJsonConsole((x) =>
+    {
+        x.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
+        {
+            Indented = true
+        };
+    });
     builder.AddProvider(new CustomLoggerProvider());
 });
 
@@ -13,3 +20,10 @@ var name = "Adam";
 var age = 32;
 
 logger.LogInformation("{Name} just turned {Age}", name, age);
+
+using (logger.BeginTimedOperation("Handle processing {name}", name))
+{
+    logger.LogInformation("{Name} just turned {Age}", name, age);
+
+    await Task.Delay(10);
+}
